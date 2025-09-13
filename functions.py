4f69fa7f2b05f9f5103185bf48e8f2cf8f47d8f8
ltrillat2025 <ltrillat2025@alu.uct.cl>
2025-09-13 20:53:48 -0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,88 @@
+import matplotlib.pyplot as plt
+import sympy as sp
+from functions import *
+
+class function:
+
+    # -- Determine if the given function is rational or not -- #
+    def isRational(self, expr):
+
+        # Sympy parsing
+        x = sp.symbols('x')
+        numerator, denominator = sp.fraction(expr)
+        # Find the zeroes
+        zeros = sp.solve(denominator, x)
+        # Filter the ones where the denominator gets to 0
+        asymp = [z for z in zeros if numerator.subs(x, z) != 0]
+        # Return True or False depending on the case
+        return len(asymp) > 0
+
+    # -- Constructor-- #
+    def __init__(self, area, expr=None, range_=300, step=0.2):
+        self.area = area
+        self.range = range_
+        self.step = step
+        self.expression = expr
+        self.line = None
+         
+        if expr is not None:
+            self.plotFunction()
+
+    # -- Logic to display the function -- #
+    def plotFunction(self):
+        x = sp.symbols('x')
+        expr = sp.sympify(self.expression)
+
+        # For some reason, this is 10x times faster than sympify, so it's important for performance
+        f_py = sp.lambdify(x, expr, 'math')
+
+        # Checks if the given function is rational or not to change the logic's behaviour 
+        # In case it's rational, it needs to have more accurate coordenates to display the asymptote,
+        # EX: If the asympote is in x = 2. 
+        # Then the program has to display points on coordenates getting close to x = 2, like x = 1.99999999.
+        if self.isRational(expr):
+            self.range = 200
+            self.step = 0.005
+
+        # -- GET X VALUES -- # 
+        # # Equidistant points that go from -range to range, with (step) many divisions
+        points = int(2*self.range / self.step) + 1
+        x_vals = [round(-self.range + i * self.step, 4) for i in range(points)]
+
+        # -- GET Y VALUES -- #
+        # Evaluates each X value, and adds it to the list. Unless it's None.
+        y_vals = []
+        for xv in x_vals:
+            try:
+                y_vals.append(f_py(xv))
+            except (ZeroDivisionError, ValueError):
+                y_vals.append(None)
+
+        # Plots the function
+        self.line, = self.area.plot(x_vals, y_vals, label=self.expression, lw=2.5)
+
+        # -- INTERSECTIONS -- # 
+        
+        # -- Y -- #
+        # Polinomic and rational functions only have one Y intersection, so it's easier 
+        # evaluate the expression on X = 0
+        y_intersec = f_py(0)
+        try:
+            self.area.plot([0], [y_intersec], 'o', label=f"Intersección Y:(0,{round(y_intersec, 4)})", color='green')
+        except:
+            pass
+
+        # -- X -- #
+        x_intersec = sp.solve(expr, x)
+        x_points = []
+        for intersec in x_intersec:
+            intersec_eval = intersec.evalf()
+            if intersec_eval.is_real:
+                x_points.append(float(intersec_eval))
+
+        # Plots all the intersections
+        for i in x_points:
+            self.area.plot([i], [0], 'o', label=f"Intersección X: ({round(i,4)},0)", color='green')
+
+        self.area.legend()
+
---
+++ main.py
@@ -0,0 +1,148 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Button, TextBox
+import sympy as sp
+from procedures import *
+from functions import *
+
+##---------- SETTING UP THE WINDOW ----------##
+
+mpl.rcParams['toolbar'] = 'None' #Hides the default toolbar
+window, function_area = plt.subplots(figsize=(1700/100, 960/100), dpi=100) #Creates the window and the function area window
+function_area.set_position([0.06, 0.15, 0.55, 0.8]) #Adjust the window's placement
+
+## -- Setting up the GUI axes/positions -- ##
+GUI_axes= {
+    "funcion" : plt.axes([0.14, 0.05, 0.15, 0.03]), "punto" : plt.axes([0.34, 0.05, 0.09, 0.03]),
+     "zoom-in" : plt.axes([0.47, 0.05, 0.05, 0.03]),
+    "zoom-out" : plt.axes([0.55, 0.05, 0.05, 0.03])
+}
+
+## -- Two Input boxes -- ##
+function_input = TextBox(GUI_axes["funcion"],label="Función: ")
+evaluate_input = TextBox(GUI_axes["punto"],label="X= ")
+
+## -- Zoom In and Out buttons -- ##
+zoomIn_button = Button(GUI_axes["zoom-in"], label="zoom-in")
+zoomOut_button = Button(GUI_axes["zoom-out"], label="zoom-out")
+
+## -- Creates the console that shows the entire procedure -- ## 
+console = window.text(
+    0.63, 0.945,  
+    "a",
+    va='top',
+    ha='left',
+    fontdict={'family':'monospace', 'size':12, 'weight':'bold', 'color':'darkblue'},
+    wrap=True,
+    multialignment='left',
+    bbox={'facecolor':'white', 'alpha':0.8, 'edgecolor':'black'}
+)
+initialText = 'Para graficar una función, el formato es el siguente \n -Exponencial: "x**2" \n -Multiplicacion y División: "x*2" y "x/2" \n -Suma "x+2"\n\nPara operar polinomios, importante utilizar paréntesis \n Ejemplo (x**2+10*x+30)/(x+15) \n\nFunciones aceptadas: Polinómicas y Racionales\n\n\n'
+
+
+##---------- USER INTERACTION ----------##
+
+zoom = 100
+displayFunction = None
+displayPoint = None
+
+## -- Creating the Function for Each button/submit
+def FunctionSubmit(texto):
+    global zoom, displayFunction, initialText
+
+    ##Clears up the area
+    function_area.cla()
+
+    #Shows the (0,0) Lines for reference
+    function_area.axhline(0, color='black', linewidth=1)  
+    function_area.axvline(0, color='black', linewidth=1)  
+
+    #Resets the zoom
+    function_area.set_xlim(-zoom, zoom)
+    function_area.set_ylim(-zoom, zoom)
+
+    #Creates the function
+    try:
+        displayFunction = function(expr=texto, area=function_area)
+    except:
+        pass
+
+    # Draws the grid again
+    function_area.grid(True)
+    function_area.figure.canvas.draw()
+
+    # Shows the procedures (calling procedures.py)
+    try:
+        console.set_text(initialText + fullExplanation(displayFunction.expression))
+    except:
+        pass
+
+    plt.show()
+
+def EvaluateSubmit(texto):
+    global displayFunction, displayPoint
+
+    ## Clears up the area
+    function_area.cla()
+
+    ## Finds the expression
+    x = sp.symbols('x')
+    expr = sp.sympify(displayFunction.expression)
+
+    ## Stores X and Y values
+    try:
+        point_x = float(texto)
+        point_y = expr.subs(x, point_x).evalf()
+    except:
+        pass
+
+    ## Re-Displays the function
+    # (This isn't optimal, but I didn't find any other way)
+    FunctionSubmit(expr)
+
+    ## Displays the point evaluated
+    try:
+        displayPoint = function_area.plot([point_x], [point_y], 'ro', label=f"({point_x},{round(float(point_y),2)})")
+        function_area.legend()
+    except:
+        pass
+
+def ZoomIn(event):
+    global zoom
+    # If greater than minimum zoom level, decrease the range of visibility #
+    if zoom > 5:
+        zoom-=5
+    
+    # Adjust the window to the zoom level #
+    function_area.set_xlim(-zoom, zoom)
+    function_area.set_ylim(-zoom, zoom)
+    plt.show()
+
+def ZoomOut(event):
+    global zoom
+    # If less than maximum zool level, increase the range of visibility #
+    if zoom < 1000:
+        zoom += 5
+    function_area.set_xlim(-zoom, zoom)
+    function_area.set_ylim(-zoom, zoom)
+    plt.show()
+
+## -- Assigning each function to each button/submit
+function_input.on_submit(FunctionSubmit)
+evaluate_input.on_submit(EvaluateSubmit)
+zoomIn_button.on_clicked(ZoomIn)
+zoomOut_button.on_clicked(ZoomOut)
+
+# -- Initial Display
+console.set_text(initialText)
+function_area.set_xlim(-zoom, zoom)
+function_area.set_ylim(-zoom, zoom)
+function_area.axhline(0, color='black', linewidth=1)  
+function_area.axvline(0, color='black', linewidth=1)  
+function_area.grid(True)
+function_area.figure.canvas.draw()
+plt.legend()
+plt.show()
+
+
+
